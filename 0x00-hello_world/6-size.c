3a30ccf2c2a8380d2ef9f86acee1949f849e8736
DanielMutunga <mutungadaniel98@gmail.com>
2023-02-16 10:25:32 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,16 +3,13 @@
  *main - Entry point
  *Return: Always 0 (success)
  */
-
 int main(void)
 {
 	char a;
 	int b;
 	float c;
-	
-printf("size of char: %lu bytes(s)\n", sizeof(a));
-printf("size of char: %lu bytes(s)\n", sizeof(b));
-printf("size of char: %lu bytes(s)\n", sizeof(c));
+printf("size of a char: %lu bytes(s)\n", sizeof(a));
+printf("size of an int: %lu bytes(s)\n", sizeof(b));
+printf("size of a float: %lu bytes(s)\n", sizeof(c));
 return (0);
-
 }
