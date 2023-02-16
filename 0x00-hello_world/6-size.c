39b0b357aab812c07f566f50ccae288c9f7c2836
DanielMutunga <mutungadaniel98@gmail.com>
2023-02-16 10:07:34 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main -Entry point
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("size of a char: %zu bytes(s)\n", sizeof(char));
+	printf("size of an int: %zu bytes(s)\n", sizeof(int));
+	printf("size of a float: %zu bytes(s)\n", sizeof(float));
+	return (0);
+}
