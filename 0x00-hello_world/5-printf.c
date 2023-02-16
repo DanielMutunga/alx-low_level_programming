2a5f73c80eb6438ee356ea06347595696eec1274
DanielMutunga <mutungadaniel98@gmail.com>
2023-02-16 09:54:01 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main- A program
+ *main - Entry piont
  *
- * Return:Always 0 (success)
+ *Return:Always 0 (success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
