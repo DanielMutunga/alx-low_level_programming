7ca05967269d7399d2a4435f4d752ba5dea55c51
DanielMutunga <mutungadaniel98@gmail.com>
2023-02-27 03:37:41 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints part of a string
+ * @str: string to be printed
+ *
+ * Return: void
+ **/
+void puts2(char *str)
+{
+int y = 0;
+while (str[y] != '\0')
+{
+if (y % 2 == 0)
+_putchar(str[y]);
+i++;
+}
+
+_putchar('\n');
+}
+
